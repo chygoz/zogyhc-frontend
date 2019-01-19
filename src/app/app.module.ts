93bcdd418516304cc224ecf28bcacf6df2203033
Sai Kiran <sainik18@gmail.com>
2019-01-19 15:50:11 +0530

    code dump

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,25 @@
-
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app-routing.module';
-import { SharedModule } from "./shared/shared.module";
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { AppRoutingModule } from './app.routes';
 import { AppComponent } from './app.component';
-import { ContentLayoutComponent } from "./layouts/content/content-layout.component";
-import { FullLayoutComponent } from "./layouts/full/full-layout.component";
-
-import { AuthService } from './shared/auth/auth.service';
-import { AuthGuard } from './shared/auth/auth-guard.service';
-
-import * as $ from 'jquery';
-import { RegisterComponent } from './register/register.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
-import { AboutusComponent } from './aboutus/aboutus.component';
-
-
-
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        FullLayoutComponent,
-        ContentLayoutComponent,
-        RegisterComponent,
-        HomeComponent,
-        AboutusComponent
-    ],
-    imports: [
-        BrowserAnimationsModule,
-        AppRoutingModule,
-        SharedModule,
-        NgbModule.forRoot()
-    ],
-    providers: [
-        AuthService,
-        AuthGuard
-    ],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    AboutUsComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
