841d62e9b7d4d1a89b3efdfbae04d062be2769aa
saisudhakar <muvvasaisudhakar@gmail.com>
2019-02-14 23:33:45 +0530

    header added
    code committed by saisudhakar.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+
+  }
 
   ngOnInit() {
+    var $menu= $('#main_menu');
+    $('.hamburger').on('click', function () {
+      $menu.toggleClass('show');
+      $menu.find('nav').toggleClass('animated fadeIn');
+      $menu.find('.follow_us').toggleClass('animated fadeInUp');
+      $('.header').toggleClass('sticky_menu_active');
+    });
+    var hamburgers = document.querySelectorAll(".hamburger");
+    // hamburgers.addEventListener('click', function() {
+    //   this.classList.toggle('is-active');
+    // }
+   
   }
+  status: boolean = false;
+  toggleclick(){
+    this.status = !this.status;       
+}
 
 }
